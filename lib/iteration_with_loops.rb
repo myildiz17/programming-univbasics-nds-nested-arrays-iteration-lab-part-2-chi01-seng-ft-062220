a9e52a93694a7ae2d402383528e0500ad9675177
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-01 06:15:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ row_index=0
 while row_index<src.count do
   element_index=0
   lowes_value= 100000
-  while element_index<src.count[row_index] do
+  while element_index<src[row_index].count do
     if src[row_index][element_index]<lowes_value
       lowes_value = src[row_index][element_index]
     end
